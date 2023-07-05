68b01d463c4e633d4a3cc945254f6fbe4040c69d
Anjana Vakil <anjanavakil@gmail.com>
2023-07-04 21:40:26 -0700

    Try to get tailwind config working in Codux (no luck)

---
+++ tailwind.config.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        blue: "#495CEF",
+        yellow: "#FFC328",
+        "gray-dark": "#8D8D8D",
+        "gray-med": "#D9D9D9",
+        "gray-light": "#F3F3F3",
+      },
+    },
+  },
+  plugins: [],
+};
