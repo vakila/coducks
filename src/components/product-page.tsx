af2ec0b1f0e1e9007794d134921e0ed7155d1bbe
Anjana Vakil <anjanavakil@gmail.com>
2023-07-05 00:18:31 -0700

    Add size toggle

--- src/components/product-page.tsx
+++ src/components/product-page.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import * as ToggleGroup from "@radix-ui/react-toggle-group";
 import { Images } from "./images";
 import { Icon } from "./icon";
 import type { CartItem, Product, Review } from "../types";
@@ -9,6 +11,9 @@ export const ProductPage = ({
   product: Product;
   addToCart: (item: CartItem) => void;
 }) => {
+  const [size, setSize] = useState("");
+  const item = { product, size: size, quantity: 1 };
+
   return (
     <main className="grid grid-cols-2 grid-rows-auto gap-2">
       <Images images={product.images} />
@@ -20,10 +25,12 @@ export const ProductPage = ({
         </h2>
         <RatingAverage reviews={product.reviews} />
 
-        <div className="flex justify-between">
+        <div className="flex justify-between items-end">
+          <SizeToggle sizes={product.sizes} onChange={(s) => setSize(s)} />
           <button
-            className="bg-black text-white px-5 py-2 rounded"
-            onClick={() => addToCart({ product, size: "S", quantity: 1 })}
+            className="bg-black disabled:bg-gray-dark text-white px-5 py-2 rounded font-medium"
+            disabled={!size}
+            onClick={() => addToCart(item)}
           >
             Add to Cart
           </button>
@@ -44,6 +51,38 @@ export const RatingAverage = ({ reviews }: { reviews: Review[] }) => {
       <p className="whitespace-nowrap">
         {avgRating.toFixed(2)} ({reviews.length})
       </p>
+    </div>
+  );
+};
+
+export const SizeToggle = ({
+  sizes,
+  onChange,
+}: {
+  sizes: Product["sizes"];
+  onChange: (s: string) => void;
+}) => {
+  return (
+    <div className="flex flex-col items-start gap-1 w-40  font-medium">
+      <label className="leading-tight">Size</label>
+      <ToggleGroup.Root
+        type="single"
+        aria-label="Product size"
+        onValueChange={onChange}
+        className="w-full grid grid-flow-col bg-gray-light rounded"
+      >
+        {sizes.map((size, i) => (
+          <ToggleGroup.Item
+            className="bg-gray-light data-[state=on]:bg-yellow px-5 py-2 rounded"
+            key={i}
+            value={size}
+            title={`Size ${size}`}
+            aria-label={`Size ${size}`}
+          >
+            {size}
+          </ToggleGroup.Item>
+        ))}
+      </ToggleGroup.Root>
     </div>
   );
 };
