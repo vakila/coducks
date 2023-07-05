e3e39047554bb34a35263a3d7866e4cb39fa0b6b
Anjana Vakil <anjanavakil@gmail.com>
2023-07-04 23:43:18 -0700

    Add product page header

--- src/components/product-page.tsx
+++ src/components/product-page.tsx
@@ -5,7 +5,13 @@ export const ProductPage = ({ product }: { product: Product }) => {
   return (
     <main className="grid grid-cols-2 grid-rows-auto gap-2">
       <Images images={product.images} />
-      <h2 className="text-[28px] p-5">{product.name}</h2>
+
+      <div className="flex flex-col gap-5 p-5 pr-10">
+        <h2 className="flex justify-between text-3xl font-normal">
+          <span className="w-1/2">{product.name}</span>
+          <span>${product.price}</span>
+        </h2>
+      </div>
     </main>
   );
 };
