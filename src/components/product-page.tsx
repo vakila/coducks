c15494def1576e3e434c2c2d9c0f73b4abcaf08a
Anjana Vakil <anjanavakil@gmail.com>
2023-07-04 23:46:18 -0700

    Add rating stars

--- src/components/product-page.tsx
+++ src/components/product-page.tsx
@@ -1,5 +1,6 @@
 import { Images } from "./images";
-import type { Product } from "../types";
+import { Icon } from "./icon";
+import type { Product, Review } from "../types";
 
 export const ProductPage = ({ product }: { product: Product }) => {
   return (
@@ -11,7 +12,23 @@ export const ProductPage = ({ product }: { product: Product }) => {
           <span className="w-1/2">{product.name}</span>
           <span>${product.price}</span>
         </h2>
+        <RatingAverage reviews={product.reviews} />
       </div>
     </main>
+  );
+};
+
+export const RatingAverage = ({ reviews }: { reviews: Review[] }) => {
+  if (!reviews.length)
+    return <p className="italic whitespace-nowrap">No reviews yet</p>;
+  const avgRating =
+    reviews.map((r) => r.rating).reduce((a, b) => a + b) / reviews.length;
+  return (
+    <div className="flex items-center gap-3">
+      <Icon name={`Stars-${Math.round(avgRating)}`} />
+      <p className="whitespace-nowrap">
+        {avgRating.toFixed(2)} ({reviews.length})
+      </p>
+    </div>
   );
 };
