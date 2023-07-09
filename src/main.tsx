04c45297950f6f3a7d9a4eb107c87547bad3798a
Anjana Vakil <anjanavakil@gmail.com>
2023-07-08 18:12:56 -0700

    Fix ts warnings

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./tailwind.css";
 import "./reset.css";
-import App from "./App.tsx";
-import { RUBBER_DUCKY, SCROOGE } from "./data.ts";
+import App from "./App";
+import { RUBBER_DUCKY, SCROOGE } from "./data";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
