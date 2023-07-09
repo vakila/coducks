cc25026ffa2935496eb2f320d83ea2ddf3ebc368
Anjana Vakil <anjanavakil@gmail.com>
2023-07-08 18:45:33 -0700

    Set width on header boards

--- src/_codux/boards/header-cart-empty.board.tsx
+++ src/_codux/boards/header-cart-empty.board.tsx
@@ -1,9 +1,14 @@
-import { createBoard } from '@wixc3/react-board';
-import { Header } from '../../components/header';
+import { createBoard } from "@wixc3/react-board";
+import { Header } from "../../components/header";
 
 export default createBoard({
-    name: 'Header (Cart empty)',
-    Board: () => <div>
-        <Header cart={[]}  />
+  name: "Header (Cart empty)",
+  Board: () => (
+    <div>
+      <Header cart={[]} />
     </div>
+  ),
+  environmentProps: {
+    canvasWidth: 800,
+  },
 });
--- src/_codux/boards/header.board.tsx
+++ src/_codux/boards/header.board.tsx
@@ -1,10 +1,13 @@
 import { createBoard } from '@wixc3/react-board';
 import { Header } from '../../components/header';
-import {RUBBER_DUCKY, SCROOGE} from '../../data'
+import { RUBBER_DUCKY, SCROOGE } from '../../data'
 
 export default createBoard({
     name: 'Header',
     Board: () => <div>
-        <Header cart={[{product: RUBBER_DUCKY, size: "S", quantity: 2}]} user={SCROOGE} />
-    </div>
+        <Header cart={[{ product: RUBBER_DUCKY, size: "S", quantity: 2 }]} user={SCROOGE} />
+    </div>,
+    environmentProps: {
+        canvasWidth: 800
+    }
 });
