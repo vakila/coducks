04757d099f5e81cecdd481ae9ca7f2189d01497a
Anjana Vakil <anjanavakil@gmail.com>
2023-07-09 20:17:54 -0700

    2.2 Radix: size toggle

--- src/components/product-info.tsx
+++ src/components/product-info.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import * as ToggleGroup from "@radix-ui/react-toggle-group";
 import { RatingAverage } from "./reviews";
 import type { CartItem, Product } from "../types";
 
@@ -8,7 +10,7 @@ export const ProductInfo = ({
   product: Product;
   addToCart: (item: CartItem) => void;
 }) => {
-  const size = product.sizes[0];
+  const [size, setSize] = useState("");
   const item = { product, size: size, quantity: 1 };
 
   return (
@@ -20,7 +22,7 @@ export const ProductInfo = ({
       <RatingAverage reviews={product.reviews} />
 
       <div className="flex justify-between items-end">
-        <p>Size {size}</p>
+        <SizeToggle sizes={product.sizes} onChange={(s) => setSize(s)} />
         <button
           className="bg-black disabled:bg-gray-dark text-white px-5 py-2 rounded font-medium"
           disabled={!size}
@@ -31,6 +33,38 @@ export const ProductInfo = ({
       </div>
 
       <p>{product.description}</p>
+    </div>
+  );
+};
+
+export const SizeToggle = ({
+  sizes,
+  onChange,
+}: {
+  sizes: Product["sizes"];
+  onChange: (s: string) => void;
+}) => {
+  return (
+    <div className="flex flex-col items-start gap-1 w-40  font-medium">
+      <label className="leading-tight">Size</label>
+      <ToggleGroup.Root
+        type="single"
+        aria-label="Product size"
+        onValueChange={onChange}
+        className="w-full grid grid-flow-col bg-gray-light rounded"
+      >
+        {sizes.map((size, i) => (
+          <ToggleGroup.Item
+            className="bg-gray-light data-[state=on]:bg-yellow px-5 py-2 rounded cursor-pointer"
+            key={i}
+            value={size}
+            title={`Size ${size}`}
+            aria-label={`Size ${size}`}
+          >
+            {size}
+          </ToggleGroup.Item>
+        ))}
+      </ToggleGroup.Root>
     </div>
   );
 };
