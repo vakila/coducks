7fdca2715eaf3f0224489c1ff5e8322e9271e771
Anjana Vakil <anjanavakil@gmail.com>
2023-07-09 19:14:44 -0700

    [revert] Tailwind: product info

--- src/components/product-info.tsx
+++ src/components/product-info.tsx
@@ -1,5 +1,3 @@
-import { useState } from "react";
-import * as ToggleGroup from "@radix-ui/react-toggle-group";
 import { RatingAverage } from "./reviews";
 import type { CartItem, Product } from "../types";
 
@@ -10,61 +8,25 @@ export const ProductInfo = ({
   product: Product;
   addToCart: (item: CartItem) => void;
 }) => {
-  const [size, setSize] = useState("");
+  const size = product.sizes[0];
   const item = { product, size: size, quantity: 1 };
 
   return (
-    <div className="flex flex-col gap-5 p-5">
-      <h2 className="flex justify-between text-3xl font-normal">
-        <span className="w-2/3">{product.name}</span>
+    <div className="">
+      <h2 className="">
+        <span className="">{product.name}</span>
         <span>${product.price}</span>
       </h2>
       <RatingAverage reviews={product.reviews} />
 
-      <div className="flex justify-between items-end">
-        <SizeToggle sizes={product.sizes} onChange={(s) => setSize(s)} />
-        <button
-          className="bg-black disabled:bg-gray-dark text-white px-5 py-2 rounded font-medium"
-          disabled={!size}
-          onClick={() => addToCart(item)}
-        >
+      <div className="">
+        <p>Size {size}</p>
+        <button className="" disabled={!size} onClick={() => addToCart(item)}>
           Add to Cart
         </button>
       </div>
 
       <p>{product.description}</p>
-    </div>
-  );
-};
-
-export const SizeToggle = ({
-  sizes,
-  onChange,
-}: {
-  sizes: Product["sizes"];
-  onChange: (s: string) => void;
-}) => {
-  return (
-    <div className="flex flex-col items-start gap-1 w-40  font-medium">
-      <label className="leading-tight">Size</label>
-      <ToggleGroup.Root
-        type="single"
-        aria-label="Product size"
-        onValueChange={onChange}
-        className="w-full grid grid-flow-col bg-gray-light rounded"
-      >
-        {sizes.map((size, i) => (
-          <ToggleGroup.Item
-            className="bg-gray-light data-[state=on]:bg-yellow px-5 py-2 rounded cursor-pointer"
-            key={i}
-            value={size}
-            title={`Size ${size}`}
-            aria-label={`Size ${size}`}
-          >
-            {size}
-          </ToggleGroup.Item>
-        ))}
-      </ToggleGroup.Root>
     </div>
   );
 };
